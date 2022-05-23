6394f2d9605509b99169aa6b86b839f60c36a4da
Bassam Hesham <bssam2012@gmail.com>
2022-05-23 14:00:08 +0200

    Add Delete and View specific User

--- StoryBooks/routes/stories.js
+++ StoryBooks/routes/stories.js
@@ -44,6 +44,52 @@ router.get("/",ensureAuth, async (req,res)=>{
     }
 })
 
+// @desc Show Public Stories
+// @route GET /stories
+router.get("/user/:userId",ensureAuth, async (req,res)=>{
+    try {
+        const stories = await Story.find({
+            user:req.params.userId,
+            status:'public'
+        })
+        .populate('user')
+        .sort({createdAt:'desc'})
+        .lean()
+        res.render("stories/index",{
+            stories
+        })
+    } catch (error) {
+        console.error(error);
+        res.render('error/500')
+    }
+})
+
+// @desc GET Storiey
+// @route GET /stories/:id
+router.get("/:id",ensureAuth, async (req,res)=>{
+    try {
+        const story = await Story.findById({
+            _id:req.params.id
+        })
+        .populate("user")
+        .lean()
+        if(!story){
+            return res.error("error/404")
+        }
+        if(story.user._id != req.user.id){
+            res.redirect("/stories")
+        }else{
+            res.render("stories/show",{
+                story
+            })
+        }
+        
+    } catch (error) {
+        console.error(error);
+        res.render('error/500')
+    }
+})
+
 // @desc Show Edit Page Stories
 // @route GET /stories/edit/:id
 router.get("/edit/:id",ensureAuth, async (req,res)=>{
@@ -93,6 +139,18 @@ router.put("/:id",ensureAuth, async (req,res)=>{
             res.redirect('../dashboard')
         }
         
+    } catch (error) {
+        console.error(error);
+        res.render('error/500')
+    }
+})
+
+// @desc Delete story
+// @route DELETE /stories/:id
+router.delete("/:id",ensureAuth, async (req,res)=>{
+    try {
+        await Story.remove({_id:req.params.id})
+        res.redirect('../dashboard')
     } catch (error) {
         console.error(error);
         res.render('error/500')
